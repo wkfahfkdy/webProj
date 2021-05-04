d7be326ee042f816f264975266c82d7fd54fc7ff
admin <admin@YD02-14>
2021-05-04 12:55:02 +0900

    2021.05.04 (JS) 12:54

--- webProj/src/fileBoard/FileUploadServlet.java
+++ webProj/src/fileBoard/FileUploadServlet.java
@@ -1,4 +1,4 @@
-package ajax;
+package fileBoard;
 
 import java.io.IOException;
 import java.util.Enumeration;
@@ -10,44 +10,27 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.json.simple.JSONObject;
+
 import com.oreilly.servlet.MultipartRequest;
 import com.oreilly.servlet.multipart.DefaultFileRenamePolicy;
 
-@WebServlet("/jquery/uploadServlet")
-public class UploadServlet extends HttpServlet {
+@WebServlet("/fileUploadServlet")
+public class FileUploadServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	public UploadServlet() {
-
+	public FileUploadServlet() {
+		super();
 	}
-
-	@Override
-	public void init() throws ServletException {
-		System.out.println("init call()");
-	}
-
-//	@Override
-//	protected void service(HttpServletRequest req, HttpServletResponse resp) 
-//			throws ServletException, IOException {
-//		System.out.println("service call()");
-//		Enumeration<String> enumer = req.getHeaderNames();
-//		while(enumer.hasMoreElements()) {
-//			String key = enumer.nextElement();
-//			String val = req.getHeader(key);
-//			System.out.println(key + " : " + val);
-//		}
-//	}
-	// service 메소드를 주석처리하고 실행해보면 doGet이 실행된다.
-	// 이걸 주석처리 하지않으면 doGet이던 doPost던 방식을 적어두지않으면 두 개 상관없이 이거 실행
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		System.out.println("doGet call()");
-
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
+		response.setCharacterEncoding("UTF-8");
+		
 		System.out.println("doPost call()");
 		String path = "C:/temp";
 		
@@ -55,13 +38,40 @@ public class UploadServlet extends HttpServlet {
 		path = sc.getRealPath("upload"); // 서버상경로
 		
 		MultipartRequest multi = new MultipartRequest(request, path, 8*1024*1024, "UTF-8", new DefaultFileRenamePolicy() );
-													// 요청 정보, 저장 위치,   사이즈(5MB),   인코딩 방식, 중복 이름 파일일 시 1,2,3 ... 붙히는 기능
+													// 요청 정보, 저장 위치, 용량(5MB), 인코딩 방식, 중복 이름 파일일 시 1,2,3 ... 붙히는 기능
 		Enumeration en = multi.getFileNames();
+		
+		String author = multi.getParameter("author");
+		String title = multi.getParameter("title");
+		String fineN = null;
+		
 		while(en.hasMoreElements()) {
 			String name = (String) en.nextElement();
 			String fileName = multi.getFilesystemName(name);
+			fineN = fileName;
 			System.out.println("name: " + name + ", fileName: "+ fileName);
 		}
+		
+		// 입력 후 저장된 값 가져오기
+		FileDAO dao = new FileDAO();
+		FileVO vo = new FileVO();
+		
+		vo.setAuthor(author);
+		vo.setFileName(fineN);
+		vo.setTitle(title);
+		
+		FileVO rvo = dao.getInsertKeyVal(vo);
+		
+		// 가져온 값을 json 형식으로 생성. {"num":?,"author":?,"title":?,"day":? ...}
+		JSONObject obj = new JSONObject();
+		obj.put("num", rvo.getNum());
+		obj.put("author", rvo.getAuthor());
+		obj.put("title", rvo.getTitle());
+		obj.put("day", rvo.getDay());
+		obj.put("fileName", rvo.getFileName());
+		
+		response.getWriter().print(obj);
+		
 	}
 
 }
